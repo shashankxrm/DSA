9d40ba6c6b2fb8544c772fb7fb40add61bb1360b
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-09-03 07:58:47 +0530

    added files

---
+++ Blind 75 striver/src/heap/findMedianfrmDataStream.java
@@ -0,0 +1,51 @@
+package heap;
+
+// LC # 295 : Find Median from Data Stream
+// Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and
+// the median is the mean of the two middle values.
+// For example, for arr = [2,3,4], the median is 3.
+// For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+// Implement the MedianFinder class:
+// MedianFinder() initializes the MedianFinder object.
+// void addNum(int num) adds the integer num from the data stream to the data structure.
+// double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+// Example 1:
+// Input
+// ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+// [[], [1], [2], [], [3], []]
+// Output
+// [null, null, null, 1.5, null, 2.0]
+// Explanation
+// MedianFinder medianFinder = new MedianFinder();
+// medianFinder.addNum(1);    // arr = [1]
+// medianFinder.addNum(2);    // arr = [1, 2]
+// medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+// medianFinder.addNum(3);    // arr[1, 2, 3]
+// medianFinder.findMedian(); // return 2.0
+// Constraints:
+// -105 <= num <= 105
+
+public class findMedianfrmDataStream {
+    java.util.PriorityQueue<Integer> minHeap;
+    java.util.PriorityQueue<Integer> maxHeap;
+
+    public findMedianfrmDataStream() {
+        minHeap = new java.util.PriorityQueue<>();
+        maxHeap = new java.util.PriorityQueue<>(java.util.Collections.reverseOrder());
+    }
+
+    public void addNum(int num) { // time : O(logn), space : O(n)
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() { // time : O(1), space : O(n)
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+        return maxHeap.peek();
+    }
+}
---
+++ Blind 75 striver/src/heap/kthmostfreqelement.java
@@ -0,0 +1,44 @@
+package heap;
+
+// LC # 347 : Top K Frequent Elements
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+// Example 1:
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+// Input: nums = [1], k = 1
+// Output: [1]
+
+
+public class kthmostfreqelement {
+    // bucket approach
+    public int[] topKFrequent(int[] nums, int k) { // time : O(n), space : O(n)
+        java.util.Map<Integer, Integer> map = new java.util.HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        java.util.List<Integer>[] bucket = new java.util.List[nums.length + 1];
+        for (int key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new java.util.ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = bucket.length - 1; i >= 0 && index < k; i--) {
+            if (bucket[i] != null) {
+                for (int num : bucket[i]) {
+                    res[index++] = num;
+                    if (index == k) {
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+
+}
