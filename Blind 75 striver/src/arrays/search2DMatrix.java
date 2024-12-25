1cd5344127b4ec38d49a90be0e4b000e95b92ffb
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-12-25 15:01:33 +0530

    search2DMatrix

---
+++ Blind 75 striver/src/arrays/search2DMatrix.java
@@ -0,0 +1,39 @@
+package arrays;
+
+// LC 74 : Search a 2D Matrix
+// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row.
+// Example 1:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+// Example 2:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+
+public class search2DMatrix {
+    static boolean searchMatrix(int[][] matrix, int target) { // O(log(m*n)) time and O(1) space
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m * n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midElement = matrix[mid / n][mid % n];
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+        int target = 3;
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
