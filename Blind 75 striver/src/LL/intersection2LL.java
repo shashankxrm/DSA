05c7bc4ec0f97cd5b39329f5f1ff8867cd37584f
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-12-29 16:43:25 +0530

    intersection2LL

---
+++ Blind 75 striver/src/LL/intersection2LL.java
@@ -0,0 +1,26 @@
+package LL;
+
+// LC 160 : Intersection of Two Linked Lists
+// Write a program to find the node at which the intersection of two singly linked lists begins. Return the node.
+// Example 1:
+// Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+// Output: Reference of the node with value = 8
+// Example 2:
+// Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+// Output: Reference of the node with value = 2
+
+public class intersection2LL {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+}
