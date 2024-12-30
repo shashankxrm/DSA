4ce1e17472ef90b6f01d0798f78dd6f37516d732
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-12-30 16:14:31 +0530

    minCandy

---
+++ Blind 75 striver/src/greedyAlgo/minCandy.java
@@ -0,0 +1,40 @@
+package greedyAlgo;
+
+// LC 135 : Candy
+// There are n children standing in a line. Each child is assigned a rating value given in an integer array ratings.
+// You are giving candies to these children
+// subjected to the following requirements:
+// Each child must have at least one candy.
+// Children with a higher rating get more candies than their neighbors.
+// Return the minimum number of candies you need to have to distribute the candies to the children.
+// Example 1:
+// Input: ratings = [1,0,2]
+// Output: 5
+// Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+// Example 2:
+// Input: ratings = [1,2,2]
+// Output: 4
+// Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+
+public class minCandy {
+    public int candy(int[] ratings) { // O(n) O(n)
+        int n = ratings.length;
+        int[] candies = new int[n];
+        java.util.Arrays.fill(candies, 1);
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1]){
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int sum = 0;
+        for(int candy : candies){
+            sum += candy;
+        }
+        return sum;
+    }
+}
