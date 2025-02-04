2839aeff2239d2fff887313f18681d68f7b6c813
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-02-04 17:26:10 +0530

    searchInRotatedSortedArray

---
+++ Blind 75 striver/src/BS/searchInRotatedSortedArray.java
@@ -0,0 +1,92 @@
+package BS;
+
+// LC 33 : Search in Rotated Sorted Array
+// There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+// Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting
+// array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7]
+// might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4]. Given the array nums after the rotation and an integer target,
+// return the index of target if it is in nums, or -1 if it is not in nums.
+// You must write an algorithm with O(log n) runtime complexity.
+// Example 1:
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+
+public class searchInRotatedSortedArray {
+    public int search(int[] nums, int target) { // O(logn) O(1)
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        // if you don't find a pivot that means array is not rotated.
+        // So do normal BS then.
+        if (pivot == -1){
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target){
+            return pivot;
+        }
+        if (target >= nums[0]){
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+    static int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            // 4 Cases here
+            if (mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]){
+                return mid - 1;
+            }
+            if (arr[mid] <= arr[start]){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    static int binarySearch(int[] arr, int target, int start, int end){
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]){
+                start = mid + 1;
+            } else if(target < arr[mid]){
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
+
