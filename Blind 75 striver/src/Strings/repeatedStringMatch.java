019425fbccc8fbe88b772ab8e1f8ed1fbc88ab05
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-02-05 21:20:09 +0530

    repeatedStringMatch

---
+++ Blind 75 striver/src/Strings/repeatedStringMatch.java
@@ -0,0 +1,63 @@
+package Strings;
+
+// LC 686 : Repeated String Match
+// Given two strings a and b, return the minimum number of times you should repeat string a so that string b is a substring of it.
+// If it is impossible for b to be a substring of a after repeating it, return -1.
+// Notice: string "abc" repeated 0 times is "",  repeated 1 time is "abc" and repeated 2 times is "abcabc".
+// Example 1:
+// Input: a = "abcd", b = "cdabcdab"
+// Output: 3
+// Explanation: We return 3 because by repeating a three times "abcdabcdabcd", b is a substring of it.
+// Example 2:
+// Input: a = "a", b = "aa"
+// Output: 2
+// Example 3:
+// Input: a = "a", b = "a"
+// Output: 1
+
+import java.math.BigInteger;
+
+public class repeatedStringMatch {
+    public boolean check(int index, String A, String B) {
+        for (int i = 0; i < B.length(); i++) {
+            if (A.charAt((i + index) % A.length()) != B.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int repeatedStringMatch(String A, String B) {
+        int q = (B.length() - 1) / A.length() + 1;
+        int p = 113, MOD = 1_000_000_007;
+        int pInv = BigInteger.valueOf(p).modInverse(BigInteger.valueOf(MOD)).intValue();
+
+        long bHash = 0, power = 1;
+        for (int i = 0; i < B.length(); i++) {
+            bHash += power * B.codePointAt(i);
+            bHash %= MOD;
+            power = (power * p) % MOD;
+        }
+
+        long aHash = 0; power = 1;
+        for (int i = 0; i < B.length(); i++) {
+            aHash += power * A.codePointAt(i % A.length());
+            aHash %= MOD;
+            power = (power * p) % MOD;
+        }
+
+        if (aHash == bHash && check(0, A, B)) return q;
+        power = (power * pInv) % MOD;
+
+        for (int i = B.length(); i < (q + 1) * A.length(); i++) {
+            aHash -= A.codePointAt((i - B.length()) % A.length());
+            aHash *= pInv;
+            aHash += power * A.codePointAt(i % A.length());
+            aHash %= MOD;
+            if (aHash == bHash && check(i - B.length() + 1, A, B)) {
+                return i < q * A.length() ? q : q + 1;
+            }
+        }
+        return -1;
+    }
+
+}
