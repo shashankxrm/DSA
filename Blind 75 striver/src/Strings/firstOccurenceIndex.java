5fdd2ca1074effc7a1d83b90e6ad6f155bccd590
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-02-05 21:39:23 +0530

    firstOccurenceIndex

---
+++ Blind 75 striver/src/Strings/firstOccurenceIndex.java
@@ -0,0 +1,24 @@
+package Strings;
+
+// LC 28 : Implement strStr()
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// Clarification:
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+// Example 1:
+// Input: haystack = "hello", needle = "ll"
+// Output: 2
+// Example 2:
+// Input: haystack = "aaaaa", needle = "bba"
+// Output: -1
+
+public class firstOccurenceIndex {
+    public int strStr(String haystack, String needle) { // O(n * m) O(1)
+        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
+            if(haystack.substring(i,j).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
