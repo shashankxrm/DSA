498bd4b2c061ff0d9d8f4f9072a8afebf53c0282
Shashank RM <shashankreddy0608@gmail.com>
2025-06-19 12:32:54 +0530

    subArrSumEqualsK

---
+++ Patterns/src/prefixSum/subArrSumEqualsK.java
@@ -0,0 +1,34 @@
+package prefixSum;
+
+// LC560: Subarray Sum Equals K
+// https://leetcode.com/problems/subarray-sum-equals-k/
+// Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+// Example 1:
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class subArrSumEqualsK {
+    public int subarraySum(int[] nums, int k) { // Time: O(n), Space: O(n)
+        int count = 0;
+        int sum = 0;
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0, 1); // To handle the case where a subarray starts from index 0
+
+        for (int num : nums) {
+            sum += num;
+            if (prefixSumCount.containsKey(sum - k)) {
+                count += prefixSumCount.get(sum - k);
+            }
+            prefixSumCount.put(sum, prefixSumCount.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
